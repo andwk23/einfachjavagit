03f4f6e6a1fbe52770ffb51bc097a98dda37960d
andwk23 <fuerschrott@online.de>
2023-11-15 16:53:14 +0100

    Gebräuchliche Stringaktionen 2

--- src/Strings/GebräuchlicheStringaktionen.java
+++ src/Strings/GebräuchlicheStringaktionen.java
@@ -35,11 +35,37 @@ public class GebräuchlicheStringaktionen {
 		System.out.println("String mit Methode stripleading(): '" + valueWithBlanks.stripLeading() + "'");
 		System.out.println("String mit Methode stripTrailing(): '" + valueWithBlanks.stripTrailing() + "'");
 		
-		System.out.println("\n--- Länge ermitteln mit length()---");
-		 
+		System.out.println("\n--- Länge ermitteln mit length()---");		 
 		String content = "This is a short message";
 		System.out.println("String: " + content);
 		System.out.println("Stringlänge: " + content.length());
+		
+		System.out.println("\n--- Auf leeren String prüfen ---");		 
+		String noContent = "";
+		System.out.println("Die Methode isEmpty() ergibt für '': " + noContent.isEmpty());
+		System.out.println("Die Methode isBlank() ergibt für '': " + noContent.isBlank() );
+		System.out.println("Die Methode isEmpty() ergibt für ' \\t': " + " \t".isEmpty());
+		System.out.println("Die Methode isBlank() ergibt für ' \\t': " + " \t".isBlank() );
+		System.out.println("Die Methode strip().isEmpty() ergibt für ' \\t': " + " \t".strip().isEmpty());
+		
+		System.out.println("\n--- Auf einzelne Zeichen zugreifen ---");
+		System.out.println("String: " + content);
+		System.out.println("String-Index 0 mit str.charAt(0): " + content.charAt(0) );
+		System.out.println("String-Index 2 mit str.charAt(2): " + content.charAt(2) );
+		
+		System.out.println("\n--- Teilbereiche extrahieren mit der substring()-Methode ---");
+		String info = "Dies ist ein String. Rest ABC";
+		System.out.println("String: " + info);
+		System.out.println("Ersten 4 Zeichen mit str.substring(0,4): " + info.substring(0,4));
+		System.out.println("Teilbereich 9tes bis 18tes Zeichen mit str.substring(9,19): " + info.substring(9,19));
+		System.out.println("Teilbereich ab 19tem Zeichen mit str.substring(19): " + info.substring(19));
+		
+		System.out.println("\n--- Strings wiederholen mit repeat()-Methode ---");
+		String greeting = "MOIN-";
+		System.out.println("String: " + greeting);
+		System.out.println("str.repeat(4): " + greeting.repeat(4));
+
+
 	}
 
 }
